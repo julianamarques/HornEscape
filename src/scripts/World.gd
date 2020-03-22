4327e1a5f1676e14d4fbbc2133b1482f2f8081ce
julianamarques <julianabmarques@live.com>
2020-03-22 20:36:54 -0300

    Fix game name

--- src/scripts/World.gd
+++ src/scripts/World.gd
@@ -2,8 +2,19 @@ extends Node2D
 
 var time = 0
 var ground_scene = preload("res://src/scenes/Ground.tscn")
+var list = []
+var root = ground_scene.instance()
+var current = root
 
-
-	
+func _make_scenes():
+	list.push_front(current)
+	while current:
+		if true: # check some bool expression
+			var new_node = ground_scene.instance()
+			list.push_front(new_node)
+			root.add_child(new_node)
+		list.pop_back()
+		current = null if list.empty() else list.back()
+		
 func _ready() -> void:
-	set_process(true)
+	_make_scenes()
