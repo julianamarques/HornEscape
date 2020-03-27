b8e77e29e886bd6f4ca36b52cdf87f95248c88cd
Marcos Abreu <Marcos.Abreu.The@GMail.com>
2020-03-27 17:12:17 -0300

    Fixed animations and scales

--- src/scripts/Enemy.gd
+++ src/scripts/Enemy.gd
@@ -1,26 +1,26 @@
 extends StaticBody2D
 
-var flip = true
+#var flip = false
 var initial_position
 var final_position
-var velocity = 2
+var velocity = 0.2
 
 func _ready() -> void:
 	$AnimatedSprite.play("run")
 	initial_position = $".".position.x
-	final_position = initial_position + 400
+	final_position = initial_position - 600
 		
 func _process(delta: float) -> void:
-	if initial_position <= final_position and flip:
-		$".".position.x += velocity
-		$AnimatedSprite.flip_h = false
-		
-		if $".".position.x >= final_position:
-			flip = false
-			
-	if $".".position.x >= initial_position and !flip:
+	if initial_position >= final_position:
 		$".".position.x -= velocity
 		$AnimatedSprite.flip_h = true
 		
-		if $".".position.x <= initial_position:
-			flip = true
+		#if $".".position.x >= final_position:
+		#	flip = true
+			
+	#if $".".position.x >= initial_position:
+	#	$".".position.x -= velocity
+	#	$AnimatedSprite.flip_h = true
+		
+		#if $".".position.x <= initial_position:
+		#	flip = true
--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -3,9 +3,9 @@ extends KinematicBody2D
 
 var motion = Vector2()
 const UP = Vector2(0, -1)
-const GRAVITY = 20
+const GRAVITY = 25
 const SPEED = 1
-const JUMP_HEIGHT = -550
+const JUMP_HEIGHT = -750
 
 func _physics_process(delta: float) -> void:
 	motion.y += GRAVITY
@@ -31,7 +31,7 @@ func _on_EnemyKill_body_entered(body: Node) -> void:
 	if body.name == "Enemy":
 		body.queue_free()
 
-func _on_PlayerVulnerability_body_entered(body: Node) -> void:
-	if body.name == "Enemy":
-		queue_free()
-		get_tree().call_group("score_group", "game_over")
+#func _on_PlayerVulnerability_body_entered(body: Node) -> void:
+#	if body.name == "Enemy":
+#		queue_free()
+#		get_tree().call_group("score_group", "game_over")
